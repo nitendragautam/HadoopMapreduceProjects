6722ae2d0f0f18f3832f5dfcbbf533710a32a056
Nitendra Gautam <nitendra.gautam12@gmail.com>
2017-05-28 16:06:01 -0500

    Added MapReduce driver Implementation

--- src/main/java/com/nitendragautam/mapreducelearning/wordcount/WordCount.java
+++ src/main/java/com/nitendragautam/mapreducelearning/wordcount/WordCount.java
@@ -1,7 +1,9 @@
 package com.nitendragautam.mapreducelearning.wordcount;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 
 /*
@@ -11,7 +13,16 @@ Main Class
 
 public class WordCount {
 
-	public static void main( String[] args){
+	/*
+	 * Driver Implmenetation
+	 */
+	public static void main( String[] args) throws Exception{
+		Configuration conf = new Configuration();
+		Job mapreduceJob = Job.getInstance(conf ,"Word Count");
+		mapreduceJob.setJarByClass(com.nitendragautam.mapreducelearning.wordcount.WordCount.class);
+		
+		mapreduceJob.setMapperClass(com.nitendragautam.mapreducelearning.wordcount.WordCountMapper.class);
+		
 		
 	}
 	
